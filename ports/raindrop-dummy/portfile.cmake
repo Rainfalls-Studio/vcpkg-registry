65ce2d9ec150022e29afca1b8d290fb9e4f3daa6
Alexis Magnier <aaleex3984@gmail.com>
2026-01-11 00:33:52 +0100

    updated raindrop-dummy port

--- ports/raindrop-dummy/portfile.cmake
+++ ports/raindrop-dummy/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO Rainfall-Studio/Raindrop-Dummy
-  REF v0.1.0
-  SHA512 0
+  REPO Rainfalls-Studio/Dummy
+  REF v0.1.1
+  SHA512 c98e6a65df88f0d87b2c551ce9b9be92220547ceda4e3e076f81bd4d72fcc0936b59b74149b3b7327eacafe376eb80cb6460af1b00f9884d25b1a3d12f892845
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
@@ -12,5 +12,3 @@ vcpkg_cmake_config_fixup(
   PACKAGE_NAME RaindropDummy
   CONFIG_PATH lib/cmake/RaindropDummy
 )
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
